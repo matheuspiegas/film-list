0bde424907d5bb1014ccbb2b5c9f8abd143826a6
Matheus Sales <matheusaugustopiegassales@gmail.com>
2024-03-05 18:01:54 -0300

    Criando rota movie

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { SearchPage } from "./components/SearchPage/index.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./components/Home/index.jsx";
 import GlobalStyle from "../src/Styles/GlobalStyles.jsx";
+import { Movie } from "./components/Movie/index.jsx";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/search" Component={SearchPage} />
+        <Route path="/movie/:movieId" Component={Movie} />
       </Routes>
     </BrowserRouter>
     <GlobalStyle />

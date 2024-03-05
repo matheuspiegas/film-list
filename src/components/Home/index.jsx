fb418b383d75c56db6e4707f89e4c7783ca660db
Matheus Sales <matheusaugustopiegassales@gmail.com>
2024-03-05 17:16:09 -0300

    Separando codigo por componentização e removendo importações desnecessárias

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,17 @@
-import P from "prop-types";
-import * as Styled from "./styles";
+import { Container } from "../../Styles/reusableStyles";
+import { Pagination } from "../Pagination";
+import { Movies } from "../Movies";
+
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { GrFormNext } from "react-icons/gr";
-import { GrFormPrevious } from "react-icons/gr";
 
 export const Home = () => {
   const apiUrl = import.meta.env.VITE_API_POPULAR;
-  const movieInfo = import.meta.env.VITE_MOVIE_INFO;
-  const apiKey = import.meta.env.VITE_API_KEY;
-  const imgUrl = import.meta.env.VITE_IMG_URL;
 
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
-  const fetchDiscover = async () => {
+  const fetchDiscover = async (page) => {
     const response = await fetch(`${apiUrl}${page}`);
     const data = await response.json();
     setTotalPages(data.total_pages);
@@ -23,7 +19,7 @@ export const Home = () => {
   };
   const { data, isLoading, isError } = useQuery({
     queryKey: ["discover", { page }],
-    queryFn: fetchDiscover,
+    queryFn: () => fetchDiscover(page),
   });
 
   const prevPage = () => {
@@ -31,44 +27,22 @@ export const Home = () => {
   };
 
   const nextPage = () => {
-    if (page === totalPages) {
-      setPage(page);
-    } else {
-      setPage(page + 1);
-    }
+    page === totalPages ? setPage(page) : setPage(page + 1);
   };
 
   return (
-    <Styled.Container>
+    <Container>
       <h1>Popular Movies</h1>
-      <Styled.ContainerMovies>
-        {!isLoading &&
-          data.results.map((m) => (
-            <Styled.MovieCard key={m.id}>
-              <Link to={`${movieInfo}${m.id}?${apiKey}`}>
-                <img src={`${imgUrl}${m.poster_path}`} alt="" />
-                <h3>{m.title}</h3>
-              </Link>
-            </Styled.MovieCard>
-          ))}
-        {isError && <p>Ocorreu um erro.</p>}
-      </Styled.ContainerMovies>
-      <Styled.ContainerPagination>
-        {!isLoading && (
-          <Styled.ContainerPages>
-            <p>{page}</p>
-            <p>{data.total_pages}</p>
-          </Styled.ContainerPages>
-        )}
-        <Styled.ContainerButtons>
-          <button onClick={prevPage}>
-            <GrFormPrevious />
-          </button>
-          <button onClick={nextPage}>
-            <GrFormNext />
-          </button>
-        </Styled.ContainerButtons>
-      </Styled.ContainerPagination>
-    </Styled.Container>
+      {isError && <p>Ocorreu um erro ao buscar dados!</p>}
+      {!isLoading && data && data.results && <Movies data={data} />}
+      {!isLoading && data && data.total_pages && (
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          nextPage={nextPage}
+          prevPage={prevPage}
+        />
+      )}
+    </Container>
   );
 };

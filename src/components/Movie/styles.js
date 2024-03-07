1c19d0246c79208d63d65dca6c1ae91724b8f6cb
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-03-07 17:39:17 -0300

    Começando estilos para mobile

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -1,7 +1,35 @@
 import styled from "styled-components";
 
-export const ContainerMovie = styled.div`
-  text-align: center;
-  margin: 2rem auto;
-  width: 500px;
+export const Container = styled.div`
+  /* height: 100vh; */
+  background-color: rgba(1, 1, 1, 0.1);
+`;
+export const Card = styled.div`
+  @media (max-width: 520px) {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    padding: 0.5rem;
+    gap: 0.3rem;
+  }
+`;
+export const Info = styled.div`
+  span {
+    font-weight: 400;
+  }
+  @media (max-width: 520px) {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+`;
+
+export const Poster = styled.img`
+  max-width: 100%;
+  height: auto;
+  @media (max-width: 520px) {
+    max-width: 30%;
+    width: 100%;
+    height: 100%;
+  }
 `;

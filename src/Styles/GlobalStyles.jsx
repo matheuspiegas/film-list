5b7bb5110a16fc0d2dbc85397537c9194cd81c68
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-03-12 10:20:36 -0300

    Reset CSS

--- src/Styles/GlobalStyles.jsx
+++ src/Styles/GlobalStyles.jsx
@@ -2,11 +2,22 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wdth,wght@0,62.5..100,100..900;1,62.5..100,100..900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  /* html {
+    font-size: 62.5%;
+  } */
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: "Noto Sans", sans-serif;
+    height: 100vh;
   }
   button {
         width: 2rem;
